be54e95e17b7c1b26da99d597ada55607790246e
gmenard <guillaume.menard95@gmail.com>
2014-08-24 23:34:39 -0700

    Initial commit

---
+++ js/validator.js
@@ -0,0 +1,43 @@
+var bracketsMap = { 
+	'(' : ')', 
+	'{' : '}', 
+	'[' : ']'
+};
+	
+var openings = Object.keys(bracketsMap);
+var endings = openings.map(function(v) { return bracketsMap[v]; });
+
+function validator(content) {
+		
+	var currentRow = 1;
+	var currentCol = 0;
+	
+	var stack = new Array();
+	for (var i = 0; i < content.length; i++) {
+		if($.inArray(content[i], openings) != -1) {
+			stack.push(content[i]);
+		}
+		else if ($.inArray(content[i], endings) != -1) {
+			if(bracketsMap[stack.pop()] != content[i]) {
+				return {
+					row : currentRow,
+					col : currentCol
+				};
+			}
+		}
+		
+		if (content[i] == '\n') {
+			currentRow++;
+			currentCol = 0;
+		}
+		currentCol++;
+	}
+	
+	if(stack.length > 0) {
+		return {
+			row : currentRow,
+			col : currentCol
+		};
+	}
+	return null;
+}
